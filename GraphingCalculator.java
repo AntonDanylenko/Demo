94e3f9d421920894b6086f817c710f08a5cc2841
AntonDanylenko <33842899+AntonDanylenko@users.noreply.github.com>
2018-01-16 12:18:19 -0500

    fixed order of textboxes

--- GraphingCalculator.java
+++ GraphingCalculator.java
@@ -7,12 +7,12 @@ public class GraphingCalculator extends JFrame implements ActionListener{
     private JPanel Constant, Linear, Quadratic, Cubic, Fourth, Fifth, Sixth, errorPanel;
     private JButton B1, B2, B3, B4, B5, B6, B7, BC1, BC2, BC3, BC4, BC5, BC6, BC7, back;
     private JTextField TF1c, TF1l , TF2l, TF1q, TF2q, TF3q, TF1cu, TF2cu, TF3cu, TF4cu, TF1fourth, TF2fourth, TF3fourth, TF4fourth, TF5fourth, TF1fifth, TF2fifth, TF3fifth, TF4fifth, TF5fifth, TF6fifth, TF1sixth, TF2sixth, TF3sixth, TF4sixth, TF5sixth, TF6sixth, TF7sixth;
-    private JLabel L1c, L1l , L2l, L1q, L2q, L3q,
-			L1cu, L2cu, L3cu, L4cu,
+    private JLabel L1c, L1l , L2l, L1q, L2q, L3q, 
+			L1cu, L2cu, L3cu, L4cu, 
 			L1fourth, L2fourth, L3fourth, L4fourth, L5fourth,
 			L1fifth, L2fifth, L3fifth, L4fifth, L5fifth, L6fifth,
 			L1sixth, L2sixth, L3sixth, L4sixth, L5sixth, L6sixth, L7sixth,
-			LH1, LH2, LH3, LH4, LH5, LH6, LH7,
+			LH1, LH2, LH3, LH4, LH5, LH6, LH7, 
 			errorMessage;
 
     public GraphingCalculator() {
@@ -28,14 +28,14 @@ public class GraphingCalculator extends JFrame implements ActionListener{
     Sixth= new JPanel();
 	errorPanel = new JPanel();
 
-    Constant.setLayout(new FlowLayout());
-    Linear.setLayout(new FlowLayout());
-    Quadratic.setLayout(new FlowLayout());
-    Cubic.setLayout(new FlowLayout());
-    Fourth.setLayout(new FlowLayout());
-    Fifth.setLayout(new FlowLayout());
-    Sixth.setLayout(new FlowLayout());
-	errorPanel.setLayout(new FlowLayout());
+    Constant.setLayout(new BoxLayout(Constant, BoxLayout.Y_AXIS));
+    Linear.setLayout(new BoxLayout(Linear, BoxLayout.Y_AXIS));
+    Quadratic.setLayout(new BoxLayout(Quadratic, BoxLayout.Y_AXIS));
+    Cubic.setLayout(new BoxLayout(Cubic, BoxLayout.Y_AXIS));
+    Fourth.setLayout(new BoxLayout(Fourth, BoxLayout.Y_AXIS));
+    Fifth.setLayout(new BoxLayout(Fifth, BoxLayout.Y_AXIS));
+    Sixth.setLayout(new BoxLayout(Sixth, BoxLayout.Y_AXIS));
+	errorPanel.setLayout(new BoxLayout(errorPanel, BoxLayout.Y_AXIS));
 
 
 
@@ -60,7 +60,21 @@ public class GraphingCalculator extends JFrame implements ActionListener{
 	back = new JButton("Back to function");
 
 
-
+    /*B2.setAlignmentX(Component.CENTER_ALIGNMENT);
+    B2.setAlignmentX(Component.CENTER_ALIGNMENT);
+    B3.setAlignmentX(Component.CENTER_ALIGNMENT);
+    B4.setAlignmentX(Component.CENTER_ALIGNMENT);
+    B5.setAlignmentX(Component.CENTER_ALIGNMENT);
+    B6.setAlignmentX(Component.CENTER_ALIGNMENT);
+    B7.setAlignmentX(Component.CENTER_ALIGNMENT);
+    BC1.setAlignmentX(Component.CENTER_ALIGNMENT);
+    BC2.setAlignmentX(Component.CENTER_ALIGNMENT);
+    BC3.setAlignmentX(Component.CENTER_ALIGNMENT);
+    BC4.setAlignmentX(Component.CENTER_ALIGNMENT);
+    BC5.setAlignmentX(Component.CENTER_ALIGNMENT);
+    BC6.setAlignmentX(Component.CENTER_ALIGNMENT);
+    BC7.setAlignmentX(Component.CENTER_ALIGNMENT);
+    back.setAlignmentX(Component.CENTER_ALIGNMENT);*/
 
     TF1c = new JTextField(5);
     TF1l = new JTextField(5);
@@ -112,42 +126,42 @@ public class GraphingCalculator extends JFrame implements ActionListener{
     LH6 = new JLabel("You are on the Fifth Degree Tab");
     LH7 = new JLabel("You are on the Sixth Degree Tab");
 
-    L1c = new JLabel("Constant Coefficient");
-    L1l = new JLabel("Constant Coefficient");
-    L1q = new JLabel("Constant Coefficient");
-    L1cu = new JLabel("Constant Coefficient");
-    L1fourth = new JLabel("Constant Coefficient");
-    L1fifth = new JLabel("Constant Coefficient");
-    L1sixth = new JLabel("Constant Coefficient");
+    L1c = new JLabel("y =");
+    L1l = new JLabel("y =");
+    L1q = new JLabel("y =");
+    L1cu = new JLabel("y =");
+    L1fourth = new JLabel("y =");
+    L1fifth = new JLabel("y =");
+    L1sixth = new JLabel("y =");
 
-    L2l = new JLabel("Linear Coefficient");
-    L2q = new JLabel("Linear Coefficient");
-    L2cu = new JLabel("Linear Coefficient");
-    L2fourth = new JLabel("Linear Coefficient");
-    L2fifth = new JLabel("Linear Coefficient");
-    L2sixth = new JLabel("Linear Coefficient");
-
-
-    L3q = new JLabel("Quadratic Coefficient");
-    L3cu = new JLabel("Quadratic Coefficient");
-    L3fourth = new JLabel("Quadratic Coefficient");
-    L3fifth = new JLabel("Quadratic Coefficient");
-    L3sixth = new JLabel("Quadratic Coefficient");
-
-    L4cu = new JLabel("Cubic Coefficient");
-    L4fourth = new JLabel("Cubic Coefficient");
-    L4fifth = new JLabel("Cubic Coefficient");
-    L4sixth = new JLabel("Cubic Coefficient");
-
-    L5fourth = new JLabel("Fourth Degree Coefficient");
-    L5fifth = new JLabel("Fourth Degree Coefficient");
-    L5sixth = new JLabel("Fourth Degree Coefficient");
+    L2l = new JLabel("x + ");
+    L2q = new JLabel("x + ");
+    L2cu = new JLabel("x + ");
+    L2fourth = new JLabel("x + ");
+    L2fifth = new JLabel("x + ");
+    L2sixth = new JLabel("x + ");
 
 
-    L6fifth = new JLabel("Fifth Degree Coefficient");
-    L6sixth = new JLabel("Fifth Degree Coefficient");
+    L3q = new JLabel("x^2 + ");
+    L3cu = new JLabel("x^2 + ");
+    L3fourth = new JLabel("x^2 + ");
+    L3fifth = new JLabel("x^2 + ");
+    L3sixth = new JLabel("x^2 + ");
 
-    L7sixth = new JLabel("Sixth Degree Coefficient");
+    L4cu = new JLabel("x^3 + ");
+    L4fourth = new JLabel("x^3 + ");
+    L4fifth = new JLabel("x^3 + ");
+    L4sixth = new JLabel("x^3 + ");
+
+    L5fourth = new JLabel("x^4 + ");
+    L5fifth = new JLabel("x^4 + ");
+    L5sixth = new JLabel("x^4 + ");
+
+
+    L6fifth = new JLabel("x^5 + ");
+    L6sixth = new JLabel("x^5 + ");
+
+    L7sixth = new JLabel("x^6 + ");
 
 	errorMessage = new JLabel("Please input only numerical coefficients");
 
@@ -223,81 +237,81 @@ public class GraphingCalculator extends JFrame implements ActionListener{
     Linear.add(LH2);
     Linear.add(BC2);
     Linear.add(L1l);
-    Linear.add(TF1l);
-    Linear.add(L2l);
     Linear.add(TF2l);
+    Linear.add(L2l);
+    Linear.add(TF1l);
     Linear.add(B2);
 
     Quadratic.add(LH3);
     Quadratic.add(BC3);
     Quadratic.add(L1q);
-    Quadratic.add(TF1q);
-    Quadratic.add(L2q);
-    Quadratic.add(TF2q);
-    Quadratic.add(L3q);
     Quadratic.add(TF3q);
+    Quadratic.add(L3q);
+    Quadratic.add(TF2q);
+    Quadratic.add(L2q);
+    Quadratic.add(TF1q);
     Quadratic.add(B3);
 
     Cubic.add(LH4);
     Cubic.add(BC4);
     Cubic.add(L1cu);
-    Cubic.add(TF1cu);
-    Cubic.add(L2cu);
-    Cubic.add(TF2cu);
-    Cubic.add(L3cu);
-    Cubic.add(TF3cu);
-    Cubic.add(L4cu);
     Cubic.add(TF4cu);
+    Cubic.add(L4cu);
+    Cubic.add(TF3cu);
+    Cubic.add(L3cu);
+    Cubic.add(TF2cu);
+    Cubic.add(L2cu);
+    Cubic.add(TF1cu);
     Cubic.add(B4);
 
 
     Fourth.add(LH5);
     Fourth.add(BC5);
     Fourth.add(L1fourth);
-    Fourth.add(TF1fourth);
-    Fourth.add(L2fourth);
-    Fourth.add(TF2fourth);
-    Fourth.add(L3fourth);
-    Fourth.add(TF3fourth);
-    Fourth.add(L4fourth);
-    Fourth.add(TF4fourth);
-    Fourth.add(L5fourth);
     Fourth.add(TF5fourth);
+    Fourth.add(L5fourth);
+    Fourth.add(TF4fourth);
+    Fourth.add(L4fourth);
+    Fourth.add(TF3fourth);
+    Fourth.add(L3fourth);
+    Fourth.add(TF2fourth);
+    Fourth.add(L2fourth);
+    Fourth.add(TF1fourth);
     Fourth.add(B5);
 
 
     Fifth.add(LH6);
     Fifth.add(BC6);
     Fifth.add(L1fifth);
-    Fifth.add(TF1fifth);
-    Fifth.add(L2fifth);
-    Fifth.add(TF2fifth);
-    Fifth.add(L3fifth);
-    Fifth.add(TF3fifth);
-    Fifth.add(L4fifth);
-    Fifth.add(TF4fifth);
-    Fifth.add(L5fifth);
-    Fifth.add(TF5fifth);
-    Fifth.add(L6fifth);
     Fifth.add(TF6fifth);
+    Fifth.add(L6fifth);
+    Fifth.add(TF5fifth);
+    Fifth.add(L5fifth);
+    Fifth.add(TF4fifth);
+    Fifth.add(L4fifth);
+    Fifth.add(TF3fifth);
+    Fifth.add(L3fifth);
+    Fifth.add(TF2fifth);
+    Fifth.add(L2fifth);
+    Fifth.add(TF1fifth);
     Fifth.add(B6);
 
     Sixth.add(LH7);
     Sixth.add(BC7);
     Sixth.add(L1sixth);
-    Sixth.add(TF1sixth);
-    Sixth.add(L2sixth);
-    Sixth.add(TF2sixth);
-    Sixth.add(L3sixth);
-    Sixth.add(TF3sixth);
-    Sixth.add(L4sixth);
-    Sixth.add(TF4sixth);
-    Sixth.add(L5sixth);
-    Sixth.add(TF5sixth);
-    Sixth.add(L6sixth);
-    Sixth.add(TF6sixth);
-    Sixth.add(L7sixth);
     Sixth.add(TF7sixth);
+    Sixth.add(L7sixth);
+    Sixth.add(TF6sixth);
+    Sixth.add(L6sixth);
+    Sixth.add(TF5sixth);
+    Sixth.add(L5sixth);
+    Sixth.add(TF4sixth);
+    Sixth.add(L4sixth);
+    Sixth.add(TF3sixth);
+    Sixth.add(L3sixth);
+    Sixth.add(TF2sixth);
+    Sixth.add(L2sixth);
+    Sixth.add(TF1sixth);
     Sixth.add(B7);
 
 	errorPanel.add(errorMessage);
@@ -306,7 +320,7 @@ public class GraphingCalculator extends JFrame implements ActionListener{
 
 
     frame.setContentPane(Constant);
-    frame.setSize(1400,400);
+    frame.setSize(400,400);
     frame.setLocation(100,100);
     frame.setVisible(true);
     }
@@ -375,6 +389,7 @@ public class GraphingCalculator extends JFrame implements ActionListener{
       f.add(test);
       f.setSize(800, 1000);
       f.setLocation(200,200);
+	  f.repaint();
       f.setVisible(true);
 	  }
 	  catch (IllegalArgumentException error){
@@ -405,6 +420,7 @@ public class GraphingCalculator extends JFrame implements ActionListener{
         f.add(test);
         f.setSize(800, 1000);
         f.setLocation(200,200);
+	  f.repaint();
         f.setVisible(true);
 		}
 	  catch (IllegalArgumentException error){
@@ -435,6 +451,7 @@ public class GraphingCalculator extends JFrame implements ActionListener{
       f.add(test);
       f.setSize(800, 1000);
       f.setLocation(200,200);
+	  f.repaint();
       f.setVisible(true);
     }
 	  catch (IllegalArgumentException error){
@@ -462,6 +479,7 @@ public class GraphingCalculator extends JFrame implements ActionListener{
         f.add(test);
         f.setSize(800, 1000);
         f.setLocation(200,200);
+	  f.repaint();
         f.setVisible(true);
       }
 	  catch (IllegalArgumentException error){
@@ -492,6 +510,7 @@ public class GraphingCalculator extends JFrame implements ActionListener{
       f.add(test);
       f.setSize(800, 1000);
       f.setLocation(200,200);
+	  f.repaint();
       f.setVisible(true);
     }
 	  catch (IllegalArgumentException error){
@@ -522,6 +541,7 @@ public class GraphingCalculator extends JFrame implements ActionListener{
       f.add(test);
       f.setSize(800, 1000);
       f.setLocation(200,200);
+	  f.repaint();
       f.setVisible(true);
     }
 	  catch (IllegalArgumentException error){
@@ -552,6 +572,7 @@ public class GraphingCalculator extends JFrame implements ActionListener{
       f.add(test);
       f.setSize(800, 1000);
       f.setLocation(200,200);
+	  f.repaint();
       f.setVisible(true);
     }
 	catch (IllegalArgumentException error){
